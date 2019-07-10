fd00b18aeedbdf24ca2acd1a9a6f7c1176fb2fa6
Confiks <confiks@scriptbase.org>
2019-07-10 14:07:26 +0200

    fix: Use bundled digidLogo image for logout button too

--- src/components/DigidLogoutButton.js
+++ src/components/DigidLogoutButton.js
@@ -1,11 +1,13 @@
 import { Component, h } from 'preact';
 
+import digidLogo from '../assets/digidLogo.png';
+
 export default class DigidLogoutButton extends Component {
   render() {
     return (
       <div style="margin-top: 50px;">
-        <a href="https://services-test.nijmegen.nl/irma/gemeente/digid/logout">
-          <img src="https://s3-eu-west-1.amazonaws.com/irma-brp/img/digid.png" />
+        <a href="/irma/gemeente/digid/logout">
+          <img src={digidLogo} />
           <h3 style="display: inline-block; position: relative; top: -15px; left: 15px;">Uitloggen bij DigiD</h3>
         </a>
       </div>
